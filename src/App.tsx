832201450c4669f3299e4c053fdd127c905cd8c0
Zhongning Li <tomli@ucdavis.edu>
2022-02-26 21:48:37 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { TestComponent } from './TestComponent';
+
+function App() {
+
+  return (
+    <TestComponent/>
+  )
+}
+
+export default App
---
+++ src/TestComponent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Stack, Button } from "@mui/material";
+
+export const TestComponent = () => {
+  return (
+    <Stack direction='column' spacing={2} sx={{ width: '50%' }}>
+      <Button variant="contained" onClick={() => { console.log("ECS") }}>
+        ECS
+      </Button>
+
+      <Button variant="outlined" onClick={() => { console.log("164") }}>
+        164
+      </Button>
+
+      <Button variant="contained" onClick={() => { console.log("Final") }}>
+        Final
+      </Button>
+    </Stack>
+  );
+}
