bdd913e561ed0b5cc2537ad1d793713d70eb92a3
lennoncc <lencruz@ucdavis.edu>
2022-02-27 16:11:47 -0800

    additional tweaks

--- src/Requirement.tsx
+++ src/Requirement.tsx
@@ -5,6 +5,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
 
 export const Requirement = () => {
   return (
@@ -21,17 +22,39 @@ export const Requirement = () => {
             alignItems="flex-start"
             spacing={2}
           >
-            <Typography variant="h4">University Requirements for Bachelor of Science</Typography>
-            <Typography variant="h5">Units Required:</Typography>
-            <Typography variant="h5">Units Applied:</Typography>
-            <Typography variant="h5">Catalog Year:</Typography>
+            <Typography variant="h5" style={{ fontWeight: 600}}>University Requirements for Bachelor of Science</Typography>
+            <Stack 
+              ml={3}
+              alignItems="flex-start"
+              direction="column"
+              justifyContent="center"
+              spacing={1}
+            >
+              <Typography variant="subtitle1">Units Required:</Typography>
+              <Typography variant="subtitle1">Units Applied:</Typography>
+              <Typography variant="subtitle1">Catalog Year:</Typography>
+            </Stack>
           </Stack>
         </AccordionSummary>
         <AccordionDetails>
+        <Stack
+            direction="column"
+            divider={<Divider orientation="horizontal" flexItem />}
+            spacing={2}
+        >
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            NOTE: the unit totals in the block headers may include excess transfer work. 
+            Refer to the 180 unit check block to verify completion of 180 units.
           </Typography>
+          <Typography>
+            Your UC GPA is currently above the 2.0 minimum required at UC Davis. 
+            This reflects current completed coursework is and is subject to change
+          </Typography>
+          <Typography>
+            One academic year (three quarters) in residence at UC Davis
+          </Typography>
+        </Stack>
+          
         </AccordionDetails>
       </Accordion>
     </div>
