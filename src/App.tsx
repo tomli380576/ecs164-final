b1480aeb0f887c9ec47a21ad18f74b6d7c628ced
Jimmy Trinh <72840090+Jimmy-Trinh@users.noreply.github.com>
2022-03-06 02:58:43 -0800

    first implementation
    #4

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { TestComponent } from './TestComponent';
+import { GE_Accordion } from './GE_Accordion';
 
 function App() {
 
   return (
-    <TestComponent/>
+    <GE_Accordion/>
   )
 }
 
