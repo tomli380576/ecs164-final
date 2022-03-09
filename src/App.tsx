fa16990add63fcaf0f59b3ac6ab0295bf56da2d7
Zhongning Li <tomli@ucdavis.edu>
2022-03-08 22:03:25 -0800

    clean up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { TestComponent } from './TestComponent';
 import { Major } from './Major';
-import { Requirement } from './Requirement'
 
 function App() {
-
   return (
     <Major/>
   )
