f321d6c83fa8b055a95a72f25696ccc7eefcb439
lennoncc <lencruz@ucdavis.edu>
2022-03-08 16:27:23 -0800

    removed extra filtering for app.tsx Requirement Accordion Fixes #5

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,11 @@ import logo from './logo.svg'
 import './App.css'
 import { TestComponent } from './TestComponent';
 import { Requirement } from './Requirement'
-import { Stack } from "@mui/material";
 
 function App() {
 
   return (
-    <div>
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={1}
-      >
-        <Requirement/>
-      </Stack>
-      
-    </div>
-    
+    <Requirement/>
   )
 }
 
